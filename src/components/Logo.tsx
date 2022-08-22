9fedfeaf2505677b30907ee32a7016c3c0bbc1c9
Dara Dermody <dara@getshogun.com>
2022-08-22 16:54:15 +0100

    Update official logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react'
-import { ImgHTMLAttributes } from 'react'
+import { ImgHTMLAttributes, useCallback, useRef, useState } from 'react'
 
 export default function Logo(props: ImgHTMLAttributes<HTMLImageElement>) {
+  const timeoutRef = useRef<NodeJS.Timeout>()
+  const [showOldLogo, setShowOldLogo] = useState(false)
+
+  const handleMouseEnter = useCallback(() => {
+    timeoutRef.current = setTimeout(() => setShowOldLogo(true), 5000)
+    return () => clearTimeout(timeoutRef.current)
+  }, [setShowOldLogo])
+
+  const handleMouseLeave = useCallback(() => {
+    clearTimeout(timeoutRef.current)
+    setShowOldLogo(false)
+  }, [setShowOldLogo])
+
   return (
     <img
-      src="assets/logo.png"
+      src={showOldLogo ? 'assets/drawn_logo.png' : 'assets/logo.svg'}
       draggable="false"
       alt="logo"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       {...props}
       style={{
         maxHeight: '100%',

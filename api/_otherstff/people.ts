8150e3e13d2e76935ab1b566532badc15ee766d5
Dara Dermody <dara@getshogun.com>
2022-08-31 15:03:56 +0100

    Add all the users

--- api/_otherstff/people.ts
+++ api/_otherstff/people.ts
@@ -12,42 +12,49 @@ export const AUTHORIZED_PEOPLE: Person[] = [
     username: 'Laura',
     firstName: 'Laura',
     lastName: 'Phelan',
-    nickname: '',
+    nickname: 'Alpha',
     photo: 'https://cdn.discordapp.com/avatars/226030480808607745/a0a84d6e365d3830b9e7db39c7356875.webp'
   },
   {
     username: 'Ariel',
     firstName: 'Ariel',
     lastName: 'Duke',
-    nickname: '',
+    nickname: 'Alphalpha',
     photo: 'https://cdn.discordapp.com/avatars/276912882304811009/04af342bb1597003065a331a74b07a07.webp'
   },
-  // {
-  //   username: 'Kian',
-  //   firstName: 'Kian',
-  //   lastName: 'Jennings',
-  //   nickname: '',
-  //   photo: 'https://cdn.discordapp.com/avatars/203831091897827328/342b0bdd3483ed87f9d175d1bcac121c.webp'
-  // },
-  // {
-  //   username: 'Mike',
-  //   firstName: 'Mike',
-  //   lastName: 'Watkins',
-  //   nickname: '',
-  //   photo: 'https://cdn.discordapp.com/avatars/203832153845399552/8a5e8ade7234c3f095a5fd8f81200c60.webp'
-  // },
-  // {
-  //   username: 'Jane',
-  //   firstName: 'Jane',
-  //   lastName: 'Coleman',
-  //   nickname: '',
-  //   photo: 'https://cdn.discordapp.com/avatars/749060246387032106/065d8fd98ad276ec49cb306f81f890ec.webp'
-  // },
-  // {
-  //   username: 'Austin',
-  //   firstName: 'Austin',
-  //   lastName: 'Texas',
-  //   nickname: '',
-  //   photo: 'https://cdn.discordapp.com/avatars/132103623092994048/a4d484b7badac09af3c0cf5422a8be42.webp'
-  // },
+  {
+    username: 'Kian',
+    firstName: 'Kian',
+    lastName: 'Jennings',
+    nickname: 'Mr. Partytime',
+    photo: 'https://cdn.discordapp.com/avatars/203831091897827328/342b0bdd3483ed87f9d175d1bcac121c.webp'
+  },
+  {
+    username: 'Mike',
+    firstName: 'Mike',
+    lastName: 'Watkins',
+    nickname: 'Cat',
+    photo: 'https://cdn.discordapp.com/avatars/203832153845399552/8a5e8ade7234c3f095a5fd8f81200c60.webp'
+  },
+  {
+    username: 'Jane',
+    firstName: 'Jane',
+    lastName: 'Coleman',
+    nickname: 'Wrangler',
+    photo: 'https://cdn.discordapp.com/avatars/749060246387032106/065d8fd98ad276ec49cb306f81f890ec.webp'
+  },
+  {
+    username: 'Austin',
+    firstName: 'Austin',
+    lastName: 'Foley',
+    nickname: 'Texas',
+    photo: 'https://cdn.discordapp.com/avatars/132103623092994048/a4d484b7badac09af3c0cf5422a8be42.webp'
+  },
+  {
+    username: 'Szymon',
+    firstName: 'Szymon',
+    lastName: 'Zielinski',
+    nickname: 'No nickname yet!',
+    photo: 'http://cdn.onlinewebfonts.com/svg/img_550782.png'
+  },
 ]
